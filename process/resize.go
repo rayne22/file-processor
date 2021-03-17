21bb4f06c852604bd7051aae9ad4ed59b513446d
Changa Lesa <changalesa8@gmail.com>
2021-03-17 14:49:52 +0200

    format check added to resize function

--- process/resize.go
+++ process/resize.go
@@ -6,6 +6,7 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"strings"
 )
 
 type UploadedImage struct {
@@ -16,6 +17,8 @@ type UploadedImage struct {
 
 func(u *UploadedImage) ResizeImage()  {
 
+	s := strings.Split(u.Name, ".")
+
 	// open image
 	file, err := os.Open(u.Name)
 	if err != nil {
@@ -23,23 +26,46 @@ func(u *UploadedImage) ResizeImage()  {
 	}
 
 	// decode image.Image
-	img, err := png.Decode(file)
-	if err != nil {
-		log.Fatal(err)
+
+	if s[1] == "jpg" || s[1] == "jpeg" || s[1] == "jpe" || s[1] == "jif" || s[1] == "jfif" || s[1] == "jfi" {
+		img, err := jpeg.Decode(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+
+		// resize
+		// and preserve aspect ratio
+		m := resize.Resize(u.Width, u.Height, img, resize.Lanczos3)
+
+		out, err := os.Create("temp/test_resized.jpg")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		// write new image to file
+		jpeg.Encode(out, m, nil)
+	} else if s[1] == "png" {
+		img, err := png.Decode(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+
+		// resize to width 1000 using Lanczos resampling
+		// and preserve aspect ratio
+		m := resize.Resize(u.Width, u.Height, img, resize.Lanczos3)
+
+		out, err := os.Create("temp/test_resized.jpg")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		// write new image to file
+		_ = png.Encode(out, m)
 	}
-	file.Close()
 
-	// resize to width 1000 using Lanczos resampling
-	// and preserve aspect ratio
-	m := resize.Resize(u.Width, u.Height, img, resize.Lanczos3)
-
-	out, err := os.Create("temp/test_resized.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer out.Close()
-
-	// write new image to file
-	jpeg.Encode(out, m, nil)
 	
 }
