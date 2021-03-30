2b26072844ec93a800efeac7bc3ddba0e16a3048
Changa Lesa <changalesa8@gmail.com>
2021-03-30 15:37:52 +0200

    deleted comments

--- main.go
+++ main.go
@@ -1,13 +1,3 @@
 package main
 
-func main() {
-	//r := process.UploadedImage{
-	//	ImageName:   "123.PNG",
-	//	Width:  0,
-	//	Height: 0,
-	//	Path: "temp",
-	//}
-	//
-	//
-	//r.ResizeImage()
-}
+func main() {}
